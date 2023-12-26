0c9f34d88be429b47123fd6c0fcfcd8535fb5099
Anirban Dutta <anirbandutta0@gmail.com>
2023-12-25 23:22:34 -0500

    Made a preliminary contact section

--- js/index.js
+++ js/index.js
@@ -19,12 +19,66 @@ window.addEventListener(
       }
 
       //       Timeline progress
-      timeline.style.background = `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) ${
-        trackers[i].offsetTop + 5
-      }px, var(--color-grey) ${
-        trackers[i].offsetTop + 5
-      }px, var(--color-grey) 100%)`;
+      timeline.style.background = `linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) ${trackers[i].offsetTop + 5
+        }px, var(--color-grey) ${trackers[i].offsetTop + 5
+        }px, var(--color-grey) 100%)`;
     });
   },
   { passive: true }
 );
+
+// Contact form
+function validateForm() {
+  var n = document.getElementById('name').value;
+  var e = document.getElementById('email').value;
+  var s = document.getElementById('subject').value;
+  var m = document.getElementById('message').value;
+  var onlyLetters = /^[a-zA-Z\s]*$/;
+  var onlyEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (n == "" || n == null) {
+    document.getElementById('nameLabel').innerHTML = ('Please enter your name');
+    document.getElementById('name').style.borderColor = "red";
+    return false;
+  }
+
+  if (!n.match(onlyLetters)) {
+    document.getElementById('nameLabel').innerHTML = ('Please enter only letters');
+    document.getElementById('name').style.borderColor = "red";
+    return false;
+  }
+
+  if (e == "" || e == null) {
+    document.getElementById('emailLabel').innerHTML = ('Please enter your email');
+    document.getElementById('email').style.borderColor = "red";
+    return false;
+  }
+
+  if (!e.match(onlyEmail)) {
+    document.getElementById('emailLabel').innerHTML = ('Please enter a valid email address');
+    document.getElementById('email').style.borderColor = "red";
+    return false;
+  }
+
+  if (s == "" || s == null) {
+    document.getElementById('subjectLabel').innerHTML = ('Please enter your subject');
+    document.getElementById('subject').style.borderColor = "red";
+    return false;
+  }
+
+  if (!s.match(onlyLetters)) {
+    document.getElementById('subjectLabel').innerHTML = ('Please enter only letters');
+    document.getElementById('subject').style.borderColor = "red";
+    return false;
+  }
+
+  if (m == "" || m == null) {
+    document.getElementById('messageLabel').innerHTML = ('Please enter your message');
+    document.getElementById('message').style.borderColor = "red";
+    return false;
+  }
+
+  else {
+    return true;
+  }
+}
